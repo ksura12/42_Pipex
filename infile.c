134b7e04a33e66d2b1deb65e187492a5a28d6fa1
ksura12 <kevin.sura@outlook.de>
2022-07-24 12:58:40 +0200

    pipe workes

---
+++ infile.c
@@ -0,0 +1,4 @@
+first: 1
+seecond: 1 2
+third: 1 2 3
+fourth: 1 2 3 4
--- src/main.c
+++ src/main.c
@@ -6,21 +6,21 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 13:53:36 by ksura             #+#    #+#             */
-/*   Updated: 2022/07/24 11:09:16 by ksura            ###   ########.fr       */
+/*   Updated: 2022/07/24 12:58:09 by ksura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/pipex.h"
 
-int	open_file(char *filename);
+int	open_file(char *filename, int rw);
 
 int	main(int argc, char **argv, char **envp)
 {
 	pid_t	pid;
-	// pid_t	pid2;
+	pid_t	pid2;
 	int		ends[2];
 	char	*cmd_path;
-	int		fd_infile;
+	int		fd_file[2];
 	char	**options;
 
 	if (argc != 5)
@@ -32,35 +32,44 @@ int	main(int argc, char **argv, char **envp)
 	{
 		if(pipe(ends) == -1)
 			return (1);
-		options = ft_split(argv[2], ' ');
-		cmd_path = get_cmd_path(options[0], envp);
-		fd_infile = open_file(argv[1]);
+		
+		
+		
 		pid = fork();
 		if (pid == -1)
 			return (1);
 		if (pid == 0)
 		{
-			dup2(fd_infile, STDIN_FILENO);
+			options = ft_split(argv[2], ' ');
+			cmd_path = get_cmd_path(options[0], envp);
+			fd_file[0] = open_file(argv[1], 0);
+			dup2(fd_file[0], STDIN_FILENO);
 			dup2(ends[1], STDOUT_FILENO);
 			close(ends[0]);
 			close(ends[1]);
 			execve(cmd_path, options, envp);
 			exit(-1); // if execve fails
 		}
-		// pid2 = fork();
-		// if (pid2 == -1)
-		// 	return (1);
-		// if (pid2 == 0)
-		// {
-		// 	dup2(ends[0], STDIN_FILENO);
-		// 	close(ends[0]);
-		// 	close(ends[1]);
-		// 	execve(cmd_path, options, envp);
-		// }
+		pid2 = fork();
+		if (pid2 == -1)
+			return (1);
+		if (pid2 == 0)
+		{
+			options = ft_split(argv[3], ' ');
+			cmd_path = get_cmd_path(options[0], envp);
+			fd_file[1] = open_file(argv[4], 1);
+			dup2(ends[0], STDIN_FILENO);
+			// dup2()
+			close(ends[0]);
+			close(ends[1]);
+			execve(cmd_path, options, envp);
+		}
+		close(fd_file[0]);
+		close(fd_file[1]);
 		close(ends[0]);
 		close(ends[1]);
 		waitpid(pid, NULL, 0);// WUNTRACED
-		// waitpid(pid2, NULL, 0);
+		waitpid(pid2, NULL, 0);
 		return (0);
 	}
 		
@@ -72,15 +81,18 @@ int	main(int argc, char **argv, char **envp)
 // 	options = ft_split(arg, " ");
 // }
 
-int	open_file(char *filename)
+int	open_file(char *filename, int rw)
 {
 	int fd;
 
-	fd = open(filename, O_RDONLY);
+	if (rw == 0)
+		fd = open(filename, O_RDONLY);
+	else
+		fd = open(filename, O_WRONLY);
 	if (fd == -1)
-	{
-		perror("open_file failed");
-		exit (0);
-	}
+		{
+			perror("open_file failed");
+			exit (0);
+		}
 	return (fd);
 }
